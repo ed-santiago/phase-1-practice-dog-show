cf2bcb0a77c5aac007ca571e299418dc0c82bb18
Edmond <edmond.santiago7@gmail.com>
2024-01-24 20:00:16 +1100

    render dogs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
+const tableBody = document.querySelector("#table-body");
+
+
+console.log(tableBody);
+
 fetch("http://localhost:3000/dogs")
   .then(res => res.json())
-  .then(dogs => console.log(dogs))
+  .then(dogs => dogs.forEach(dog => {
+    renderDog(dog)
+  }))
+
+function renderDog(dog) {
+  const tableRow = document.createElement("tr");
+  tableRow.innerHTML = `
+    <td>${dog.name}</td>
+    <td>${dog.breed}</td>
+    <td>${dog.sex}</td>
+    <td><button>Edit Dog</button></td>
+  `
+  tableBody.append(tableRow);
+}
